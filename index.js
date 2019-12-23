c4b86183356171d518e1b3e2f5bd5d17db861621
安藤  龍治 <kuriimusichu@gmail.com>
2019-12-23 21:57:55 +0900

    データバインディングの追記

--- index.js
+++ index.js
@@ -1,47 +1,6 @@
-var app4 = new Vue({
-    el: "#app-4",
+const vm = new Vue({
+    el: "#app",
     data: {
-        todos: [
-            { text: 'Learn JavaScript' },
-            { text: 'Learn Vue' },
-            { text: 'Build something awesome' }
-        ]
-    }
-})
-
-var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: 'Hello Vue.js!'
-    },
-    methods: {
-        reverseMessage: function() {
-            this.message = this.message.split(',').reverse().join('')
-        }
-    }
-})
-
-var app6 = new Vue({
-    el: "#app-6",
-    data: {
-        message: 'Hello Vue!'
-    }
-})
-
-
-// qpp-7
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-})
-
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        groceryList: [
-            { id: 0, text: 'Vegetables' },
-            { id: 1, text: 'Cheese' },
-            { id: 2, text: 'Whatever else humans are supposed to eat' }
-        ]
+        message: "Hello Vue.js"
     }
 })
